44ceab00a4b081db16bf4a043e33bb611b89a26a
redstonegamesb <contato@redstonegames.com.br>
2026-04-06 19:44:14 -0300

    test 1.21.10

--- src/main/java/fzzyhmstrs/structurized_reborn/mixin/StructurePoolsMixin.java
+++ src/main/java/fzzyhmstrs/structurized_reborn/mixin/StructurePoolsMixin.java
@@ -11,7 +11,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(StructurePools.class)
 public class StructurePoolsMixin {
-
    /* @Inject(method = "bootstrap", at = @At(value = "HEAD"))
     private static void structurized_reborn_getBootstrapRegisterables(Registerable<StructurePool> structurePoolsRegisterable, CallbackInfo ci){
         System.out.println("Grabbed registerable");
